21ffff74473447cfceec6b5e222adc8242e8796a
Alexey Kharlamov <der@2-47.ru>
2016-07-21 16:02:13 +0300

    small fix

--- services.sh
+++ services.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-changed=$(cd services; git show --name-only --oneline | tail -n +2 | sed -e 's|^src/||;s|^vendor/src/||'| sed -e 's|/[^/]\+\.[^/]\+$|/|' | sed -e 's|/$||' | sort | uniq)
+cd services
+
+changed=$(git show --name-only --oneline | tail -n +2 | sed -e 's|^src/||;s|^vendor/src/||'| sed -e 's|/[^/]\+\.[^/]\+$|/|' | sed -e 's|/$||' | sort | uniq)
 
 
-list=$(docker run --rm -v "$PWD/services":/usr/src/services -w "/usr/src/services/" -u $(id -u) desertbit/golang-gb:alpine sh -c "gb list -f '{{.ImportPath}} {{.Package.Imports}}'")
+list=$(docker run --rm -v "$PWD":/usr/src/services -w "/usr/src/services/" -u $(id -u) desertbit/golang-gb:alpine sh -c "gb list -f '{{.ImportPath}} {{.Package.Imports}}'")
 
 
 for file in $changed; do 
