f0fdafe9b72b63b7b4e6f1acf6923e290d689f18
betrok <betrok@ya.ru>
2018-06-01 22:16:44 +0300

    use proxy

--- hooks.go
+++ hooks.go
@@ -3,8 +3,12 @@ package main
 import (
 	"fmt"
 	"github.com/phayes/hookserve/hookserve"
+	"golang.org/x/net/proxy"
 	"gopkg.in/tucnak/telebot.v1"
 	"log"
+	"net"
+	"net/http"
+	"net/url"
 	"os"
 	"os/exec"
 	"strings"
@@ -30,7 +34,15 @@ func main() {
 	server.GoListenAndServe()
 
 	// telebot init part
-	var err error
+	transport, err := TransportFromURL(os.Getenv("TG_PROXY"))
+	if err != nil {
+		log.Println(err)
+		os.Exit(1)
+	}
+	http.DefaultClient = &http.Client{
+		Transport: transport,
+	}
+	
 	bot, err = telebot.NewBot(os.Getenv("SECRET_TOKEN"))
 	if err != nil {
 		log.Println(err)
@@ -91,4 +103,52 @@ func handleTelemsg(msg telebot.Message) {
 			run(args...)
 		}
 	}
+}
+
+var DefaultProxy = ""
+// build http.Transport for given proxy URL in scheme://user:password@host:port format
+func TransportFromURL(proxyURL string) (ret *http.Transport, err error) {
+	// mostly copy of http.DefaultTransport
+	ret = &http.Transport{
+		DialContext: (&net.Dialer{
+			Timeout:   30 * time.Second,
+			KeepAlive: 30 * time.Second,
+			DualStack: true,
+		}).DialContext,
+		MaxIdleConns:          20,
+		IdleConnTimeout:       90 * time.Second,
+		TLSHandshakeTimeout:   10 * time.Second,
+		ExpectContinueTimeout: 1 * time.Second,
+	}
+	if proxyURL == "" {
+		if DefaultProxy == "" {
+			ret.Proxy = http.ProxyFromEnvironment
+			return
+		}
+		proxyURL = DefaultProxy
+	}
+	parsed, err := url.Parse(proxyURL)
+	if err != nil {
+		return nil, err
+	}
+	switch parsed.Scheme {
+	case "http", "https":
+		ret.Proxy = http.ProxyURL(parsed)
+	default:
+		// DialContext in x/net/proxy is on review for now
+		ret.DialContext = nil
+
+		var dialer proxy.Dialer
+		// correctly supports only socks5
+		dialer, err = proxy.FromURL(parsed, &net.Dialer{
+			Timeout:   30 * time.Second,
+			KeepAlive: 30 * time.Second,
+			DualStack: true,
+		})
+		if err != nil {
+			return
+		}
+		ret.Dial = dialer.Dial
+	}
+	return
 }
